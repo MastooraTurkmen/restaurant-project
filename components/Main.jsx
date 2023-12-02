36c9fd5f292d85aaa01a1949d876a43ab1805e77
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-12-02 07:39:25 +0430

    first commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,7 +3,10 @@ import React from "react"
 export default function Main() {
         return <main>
                     <h1 className="logo-container">Simple Food Restaurant</h1>
-                    <p className="info-container">We make simple food for simple people. Want a     sandwich?   You got it. A hot dog? Coming right up. Poached salmon eggs with    fire-roasted asparagus   fritters hand-glazed in locally-sourced artisanal honey?  Get out of here. Try the Fancy   Food Cafe across the street.
+                    <p className="info-container">We make simple food for simple people. Want a     
+                    sandwich? You got it. A hot dog? Coming right up. Poached salmon eggs with 
+                    fire-roasted asparagus fritters hand-glazed in locally-sourced artisanal honey? 
+                    Get out of here. Try the Fancy Food Cafe across the street.
                     </p>
                     <div className="image-container">
                          <div className="image-wrapper">
